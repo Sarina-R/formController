dc5e8c82c91b98fecff71d0d0621ccb599166e1c
Sarina-R <146827892+Sarina-R@users.noreply.github.com>
2024-05-03 13:29:03 +0330

    title deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://Sarina-R.github.io/formController/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/formController/",
+});
